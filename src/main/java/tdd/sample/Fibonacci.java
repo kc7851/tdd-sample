5b311528ec2a4de8dccf5c5d8f84b2a515086868
KiChang Shin <kc7851@gmail.com>
2019-07-13 00:39:34 +0900

    불필요 부분 주석처리
    TDD 과정을 남겨놓기 위해 삭제하지않고 주석처리.

--- src/main/java/tdd/sample/Fibonacci.java
+++ src/main/java/tdd/sample/Fibonacci.java
@@ -6,37 +6,37 @@ public class Fibonacci {
         if(nthNum <= 2) {
             return 1;
         }
-        if(nthNum == 3){
-            return fib(3-2)+fib(3-1);
-        }
-        if(nthNum == 4){
-            return fib(4-2)+fib(4-1);
-        }
+//        if(nthNum == 3){
+//            return fib(3-2)+fib(3-1);
+//        }
+//        if(nthNum == 4){
+//            return fib(4-2)+fib(4-1);
+//        }
         return fib(nthNum-2)+fib(nthNum-1);
     }
 
     public int fibonacciArray(int nthNum) {
         int[] fiboArray = new int[nthNum];
-        if(nthNum == 1){
-            fiboArray[0] = 1; // 1
-        }
-        if(nthNum == 2){
-            fiboArray[0] = 1;
-            fiboArray[1] = 1; // 1
-        }
-        if(nthNum == 3){
-            fiboArray[0] = 1;
-            fiboArray[1] = 1;
-            fiboArray[2] = fiboArray[0] + fiboArray[1]; //1+1
-            fiboArray[2] = fiboArray[2-2] + fiboArray[2-1]; //1+1
-            fiboArray[2] = fiboArray[nthNum-3] + fiboArray[nthNum-2]; //1+1
-        }
-        if(nthNum == 4){
-            fiboArray[0] = 1;
-            fiboArray[1] = 1;
-            fiboArray[2] = 2;
-            fiboArray[3] = fiboArray[nthNum-3] + fiboArray[nthNum-2]; //1+2
-        }
+//        if(nthNum == 1){
+//            fiboArray[0] = 1; // 1
+//        }
+//        if(nthNum == 2){
+//            fiboArray[0] = 1;
+//            fiboArray[1] = 1; // 1
+//        }
+//        if(nthNum == 3){
+//            fiboArray[0] = 1;
+//            fiboArray[1] = 1;
+//            fiboArray[2] = fiboArray[0] + fiboArray[1]; //1+1
+//            fiboArray[2] = fiboArray[2-2] + fiboArray[2-1]; //1+1
+//            fiboArray[2] = fiboArray[nthNum-3] + fiboArray[nthNum-2]; //1+1
+//        }
+//        if(nthNum == 4){
+//            fiboArray[0] = 1;
+//            fiboArray[1] = 1;
+//            fiboArray[2] = 2;
+//            fiboArray[3] = fiboArray[nthNum-3] + fiboArray[nthNum-2]; //1+2
+//        }
 
         for(int i=0; i<nthNum; i++){
             if(i <= 1){
